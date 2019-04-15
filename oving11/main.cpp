1044719d6529d16732caeacef6279363157b90b9
sigurdht <legotull.99@gmail.com>
2019-04-15 10:11:29 +0200

    ferdig

--- oving11/main.cpp
+++ oving11/main.cpp
@@ -1,10 +1,51 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <set>
 
 using namespace std;
 
+void replace(vector<string>& vec, string old, string replacement) {
+	for (vector<string>::iterator it = vec.begin(); it != vec.end(); it++) {
+		if (*it == old) {
+			it = vec.erase(it);
+			it = vec.insert(it, replacement);
+		}
+	}
+}
+
+void replace(set<string>& sett, string old, string replacement) {
+	for (set<string>::iterator it = sett.begin(); it != sett.end(); it++) {
+		if (*it == old) {
+			it = sett.erase(it);
+			it = sett.insert(it, replacement);
+		}
+	}
+}
+
+void oppg1() {
+	vector<string> ting1{ "abc", "abc", "ikke abc", "abc", "ikke abc" };
+
+	replace(ting1, "abc", "def");
+	for (vector<string>::iterator it = ting1.begin(); it != ting1.end(); it++) {
+		cout << *it << endl;
+	}
+
+	cout << "-----Set:-----" << endl;
+
+	set<string> ting2{ "abc", "abc", "ikke abc", "abc", "ikke abc" };
+	for (set<string>::iterator it = ting2.begin(); it != ting2.end(); it++) {
+		cout << *it << endl;
+	}
+
+	replace(ting2, "abc", "ghi");
+	for (set<string>::iterator it = ting2.begin(); it != ting2.end(); it++) {
+		cout << *it << endl;
+	}
+}
+
 int main() {
-	cout << "Halla, jeg heter Petter" << endl;
+	oppg1();
 
 	cin.get();
 	return 0;
