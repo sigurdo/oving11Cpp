e5367c0cb4a9f22d154a4b5c63b8500650eaa849
sigurdht <legotull.99@gmail.com>
2019-04-23 11:33:49 +0200

    Ferdig med jalla implementasjon av insert

--- oving11/LinkedList.cpp
+++ oving11/LinkedList.cpp
@@ -6,7 +6,19 @@ std::ostream & operator<<(std::ostream & os, const LinkedList::Node & node) {
 	return os;
 }
 
-LinkedList::Node* insert(LinkedList::Node* pos, const std::string& value) {
-	LinkedList::Node* nextTemp = std::move(pos->getNext());
-	LinkedList::Node ny{ value, nextTemp, pos->getPrev() };
+LinkedList::Node* LinkedList::LinkedList::insert(Node* pos, const std::string& value) {
+	//LinkedList::Node* nextTemp = std::move(pos->getNext());
+	//LinkedList::Node ny{ value, std::make_unique<LinkedList::Node>(pos), pos->getPrev() };
+	//auto s2 = std::make_unique<Node>(value, pos, pos->getPrev());
+	//return s2.get(); 
+
+	/*if (pos == head.get()) {
+		
+	}
+
+	else {*/
+		pos->prev->next = std::make_unique<Node>(value, std::move(pos->prev->next), pos->prev);
+		pos->prev = pos->prev->next.get();
+		return pos->prev;
+	//}
 }
